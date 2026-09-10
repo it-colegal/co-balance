85497f498f1a3e558fb17c23792ad90065c1e5bc
it-colegal <it.colegal@gmail.com>
2026-09-10 20:05:25 +0700

    Add auth login and logout routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,3 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['auth/login']  = 'auth/login';
+$route['auth/logout'] = 'auth/logout';
